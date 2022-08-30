0dc2c459af9a1b0e3c97071ee804d59e3bb73316
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2022-08-30 18:26:20 +0300

    вертаємо до життя

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+   
+    direction: 'horizontal',
+    loop: true,
+  
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
